ff2bfe852c19e3181316c05a038e469d7a389986
frankkhiem <khiem.ngg@gmail.com>
2021-12-06 01:45:29 +0700

    Create unlink accounts services

---
+++ services/google_services/unlink_google.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+# zato: ide-deploy=True
+
+from json import dumps, loads
+from bson import json_util
+from models import *
+# các model User, GoogleAccount, OutlookAccount, Contact, SyncContacts
+from zato.server.service import Service
+
+
+class UnlinkGoogle(Service):
+  """ Nhận request gồm accessToken lấy user tương ứng, 
+      hủy liên kết user với tài khoản google
+  """
+
+  class SimpleIO:
+    input_required = 'accessToken'
+
+  def handle(self):
+    # Khai báo đối tượng request và response của service
+    request = self.request.input
+    response = self.response
+    # Set headers tránh lỗi CORS
+    response.headers = {
+      'Access-Control-Allow-Origin' : '*',
+    }
+
+    ##############################################################################################
+    # Mọi service cần xác thực người dùng và lấy thông tin của người đều cần các dòng trong vùng #
+    accessToken = request['accessToken']
+
+    input = {
+      'accessToken': accessToken
+    }
+
+    # Gọi auth middleware xác thực người dùng thông qua accessToken
+    res_auth = self.invoke('auth-middleware.auth-middleware', input)
+
+    if not res_auth['auth']:
+      response.payload = {
+        'success': False,
+        'message': 'Unauthorized'
+      }
+      response.status_code = 401
+      return
+    
+    else:
+      # userId nhận được dùng để query dữ liệu tương ứng với người dùng request đến
+      userId = res_auth['userId']
+    ##############################################################################################
+    
+    user = User.objects(user_id = userId)[0]
+    
+    user.google = GoogleAccount()
+    user.save()
+
+    response.payload = {
+      'success': True,
+      'message': 'Unlink Google successfully'
+    }
+
+    return
